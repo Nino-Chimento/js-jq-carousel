66be021e070a737b9468c25b609055f0f3ccb5aa
Nino Chimento <saninoc84@gmail.com>
2020-01-17 12:44:58 +0100

    creazione scorrimento dall ultimo al primo figlio pallini ed img

--- main.js
+++ main.js
@@ -25,8 +25,17 @@ function nextPhoto() {
   // var circle
   var circleActive = $(".nav .active");
   var nextCircleActive = circleActive.next();
+  if (imageActive.hasClass("last")) {
+    imageActive.removeClass("active");
+    $("img").first().addClass("active");
+    circleActive.removeClass("active");
+    $(".nav .first").addClass("active")
+  }
+  // image
   imageActive.removeClass("active");
   nextImageActive.addClass("active");
+  // pallini
   circleActive.removeClass("active");
   nextCircleActive.addClass("active");
+
 }
